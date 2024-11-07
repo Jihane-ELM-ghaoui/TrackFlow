30e1b110106f47a5847765db070a26bb5fa202a1
jihanelm <jihane.elmghaoui@gmail.com>
2024-11-07 19:21:07 +0100

    Initial commit

---
+++ src/test/java/ma/ensa/StorageManager/StorageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.StorageManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
