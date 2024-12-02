cd6aea9d96075467511cdf1805b73c732dc0573e
my rachid toumi <othmanetoumi@gmail.com>
2024-12-02 21:50:57 +0100

    Landing Page sprint 1

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,14 @@
+// NotFound.js
+import React from 'react';
+import './notfound.css'
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            {/* No text or additional content */}
+        </div>
+    );
+};
+
+export default NotFound;
+
