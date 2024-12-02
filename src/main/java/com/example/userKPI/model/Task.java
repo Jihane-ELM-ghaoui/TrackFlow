a27c8feb20363f4d374b454a554097e1b5eb8200
noussa2002 <133096740+noussa2002@users.noreply.github.com>
2024-12-02 23:32:55 +0100

    Update Task.java

--- src/main/java/com/example/userKPI/model/Task.java
+++ src/main/java/com/example/userKPI/model/Task.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long taskid;
+    private Long taskId;
 
     private String taskName;
     private String taskDescription;
