8499ff79ef1323d11cc7c78956905bbf920d449d
jihanelm <121095785+jihanelm@users.noreply.github.com>
2025-01-04 23:20:01 +0100

    Update SecurityConfig.java

--- src/main/java/ma/ensa/StorageManager/config/SecurityConfig.java
+++ src/main/java/ma/ensa/StorageManager/config/SecurityConfig.java
@@ -3,22 +3,17 @@ package ma.ensa.StorageManager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-
-import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for API requests if not needed
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/protected").authenticated()
                         .anyRequest().permitAll()
@@ -30,24 +25,9 @@ public class SecurityConfig {
                             jwt.jwtAuthenticationConverter(jwtAuthenticationConverter);
                         })
                 );
-
-        // Apply CORS configuration
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()));
-
+        
         return http.build();
     }
+    
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow React app URL
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "userId"));
-        configuration.setAllowCredentials(true); // Allow credentials for authenticated requests
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply to all endpoints
-        return source;
-    }
 }
